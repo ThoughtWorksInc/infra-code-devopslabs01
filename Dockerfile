25882822c0848635ef5a200408e7b37a1a880206
Rafa Nunes <datherra@yahoo.com.br>
2016-11-24 04:20:05 -0200

    implementa labs de ansible

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,16 @@
 FROM python:2
 
 RUN apt-get update && apt-get install -y groff
-RUN pip install awscli
+RUN pip install awscli ansible credstash boto
 
 WORKDIR /iac
 COPY entrypoint.sh entrypoint.sh
 COPY cfn_example.json cfn_example.json
+COPY ansible.cfg ansible.cfg
+COPY inventory inventory
+COPY pb.yml pb.yml
+COPY ec2.py ec2.py
+COPY ec2.ini ec2.ini
 
 ENTRYPOINT ["./entrypoint.sh"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-# borrowed from Erik Doernenburg:
+# with pieces borrowed from Erik Doernenburg:
 # https://my.thoughtworks.com/groups/techops-community/blog/2015/11/17/aws-account-access-via-temporary-api-tokens#comment-38435
 ROLE=becomeAdmin
 ACCOUNT=644540006937
@@ -16,5 +16,14 @@ export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-us-east-1}
 export AWS_ACCESS_KEY_ID=${KST[0]}
 export AWS_SECRET_ACCESS_KEY=${KST[1]}
 export AWS_SESSION_TOKEN=${KST[2]}
+# boto (not 3) used by ansible depends on old vars version
+export AWS_ACCESS_KEY=${KST[0]}
+export AWS_SECRET_KEY=${KST[1]}
+export AWS_SECURITY_TOKEN=${KST[2]}
+export AWS_DELEGATION_TOKEN=${KST[2]}
+
+# retrive ssh private key from previously configured credstash
+credstash get ssh_private_key > /iac/key.pem
+chmod 400 /iac/key.pem || echo "ERROR loading private ssh key"
 
 exec $@
